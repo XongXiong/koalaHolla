f9b6dde414ffe927774dcdbd20af7ab60516f19d
Xong Xiong <xxiong3153@gmail.com>
2017-10-20 14:09:12 -0500

    fixed router

--- server/routes/koala_router.js
+++ server/routes/koala_router.js
@@ -4,13 +4,13 @@ var router = express.Router();
 var koalas = []; // <- Stored on the SERVER
 
 var pg = require('pg');
-var config = {
-  database: 'deneb', // the name of the database
-  host: 'localhost', // where is your database?
-  port: 5432, // the port number for you database, 5432 is the default
-  max: 10, // how many connections at one time
-  idleTimeoutMillis: 30000 // Close idle connections to db after
-};
+// var config = {
+//   database: 'deneb', // the name of the database
+//   host: 'localhost', // where is your database?
+//   port: 5432, // the port number for you database, 5432 is the default
+//   max: 10, // how many connections at one time
+//   idleTimeoutMillis: 30000 // Close idle connections to db after
+// };
 var poolModule = require('../modules/pool.js');
 var pool = poolModule;
 
